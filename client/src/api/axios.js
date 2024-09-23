b0224dd2ea67d1771b5cdf9ff316965c3e55b920
Santino <mellamoalroberto@gmail.com>
2024-09-23 15:11:00 -0300

    package json

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'finance-tracker-app-production.up.railway.app',
     withCredentials: true
 })
 
